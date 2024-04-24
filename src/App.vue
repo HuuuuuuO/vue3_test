5453dc01450e34579385841c4dbf6dcd7b38d7a0
HuuuuuuO <673225542@qq.com>
2024-04-24 17:42:31 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">牛牛牛
+    <Person/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Person from './components/Person.vue'
+
+  export default {
+    name:'App',
+    components:{Person}
+  }
+</script>
+
+<style>
+.app {
+background-color: aqua;
+color:brown;
+font-size: 40px;
+
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{name}}</h2>
+        <h2>姓名：{{age}}</h2>
+        <button @click="changename">修改名字</button>
+        <button @click="showTel">查看联系方式</button>
+
+    </div>
+  </template>
+  
+  <script lang="ts">
+    export default {
+      name:'Person',
+      data() {
+        return{
+            name:'林俊杰',
+            age:'18',
+            tel:'13888888888'
+        }
+
+      },
+      methods:{
+        changename(){
+            this.name='案件聚焦军'
+        },
+        showTel(){
+        alert(this.tel)
+      }
+      }
+    }
+  </script>
+  
+  <style>
+  .person {
+  background-color: rgb(53, 153, 44);
+  color:rgb(41, 11, 173);
+  font-size: 40px;
+  
+  }
+  </style>
